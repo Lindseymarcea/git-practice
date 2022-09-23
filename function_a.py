c81ddf2c41b74bfede30c6928432f62ee1c9f291
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-23 17:29:56 -0400

    dgafdfas

--- function_a.py
+++ function_a.py
@@ -1,4 +1,19 @@
+
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-fasdfgasdgadsdfadfaf
+    frequency_index = {}
+    max_frequency = -1
+    most_common_value = None
+    for num in number_list:
+        if frequency_index.get(num):
+            frequency_index[num] += 1
+        else:
+            frequency_index[num] = 1
+
+        if max_frequency < frequency_index[num]:
+            max_frequency = frequency_index[num]
+            most_common_value = num
+
+    return most_common_value
+```
