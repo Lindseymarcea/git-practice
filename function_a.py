2f2e1bb1d3998897dbe4778eab9985f194861cc9
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-23 17:53:12 -0400

    this must work please git gods

--- function_a.py
+++ function_a.py
@@ -1,11 +1,5 @@
 def most_common_value(number_list):
-<<<<<<< HEAD
 
-=======
-    """ returns the most common element of the list
-    """
-<<<<<<< HEAD
->>>>>>> dbafd3956e169a8200f881ebe025f20eb5b64820
     frequency_index = {}
     max_frequency = -1
     most_common_value = None
@@ -20,15 +14,4 @@ def most_common_value(number_list):
             most_common_value = num
 
     return most_common_value
-<<<<<<< HEAD
 
-
-if __name__ == "__main__":
-    nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
-    print(f"Most common value = {most_common_value(nums)}")
-=======
-```
->>>>>>> dbafd3956e169a8200f881ebe025f20eb5b64820
-=======
-  
->>>>>>> 6c831d713ebc5092e0f1ffb53d24c4829fb19c31
