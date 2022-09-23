946a4abf3277115371640ea47f130d20b253f1b0
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-23 17:21:58 -0400

    dfgsadfas

--- function_a.py
+++ function_a.py
@@ -1,28 +1,4 @@
 def most_common_value(number_list):
-
     """ returns the most common element of the list
     """
-   #HIYA HERE'S JAIME'S VERSION. 
-def most_common_value(number_list):
-    """ returns the most common element of the list
-
-        else:
-            frequency_index[num] = 1
-<<<<<<< HEAD
-
-        if max_frequency < frequency_index[num]:
-            max_frequency = frequency_index[num]
-            most_common_value = num
-
-    return most_common_value
-=======
->>>>>>> bb6ae4fa94c4e6cdce4c6bf2a38a01d4d4f76090
-
-        # if max_frequency < frequency_index[num]:
-        #     max_frequency = frequency_index[num]
-        #     most_common_value = num
-
-    return most_common_value
-if __name__ == "__main__":
-    nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
-    print(f"Most common value = {most_common_value(nums)}")
+  
