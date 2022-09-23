6c831d713ebc5092e0f1ffb53d24c4829fb19c31
Jaime Mitchell <JaimeMitchell100@gmail.com>
2022-09-23 17:38:18 -0400

    dafsdfsa

--- function_b.py
+++ function_b.py
@@ -2,25 +2,13 @@
 # Iterating Over Data
 # Problem-Set While Loops #11
 def silly_sum():
-    ""sdfadfasfdf
-
-def most_common_value(number_list):
-    """ returns the most common element of the list
+    """ reads numbers from the user (use input_int) 
+        summing as we go until either
+        the user enters 0, or
+        the sum reaches or exceeds 1000
     """
-    frequency_index = {}
-    max_frequency = -1
-    most_common_value = None
-    for num in number_list:
-        if frequency_index.get(num):
-            frequency_index[num] += 1
-        else:
-            frequency_index[num] = 1
+    pass
 
-        if max_frequency < frequency_index[num]:
-            max_frequency = frequency_index[num]
-            most_common_value = num
 
-    return most_common_value
-```
 if __name__ == "__main__":
     print(f"Answer = {silly_sum()}")
